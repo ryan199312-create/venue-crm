936bf906074015a97bbbaa331775940a8689d8d4
ryan199312-create <ryan199312@gmail.com>
2026-01-28 00:58:09 +0800

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.jsx 	new file:   src/VMS.jsx 	renamed:    src/Website.js -> src/Website.jsx 	modified:   src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css' // <--- ⚠️ 必須要有這一行！
+import App from './App.jsx' // This now points to your Traffic Controller
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
